9c0299b9350eb2c49b91e4bf9b000c919c2b7051
NeerNim <neernimt@gmail.com>
2019-07-31 12:58:23 +0200

    add bootstrap layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'sign_up' }
+  devise_for :users
 end
