1fd70cda8e48a54a34ff61a47c47f8d1122aeb45
NeerNim <neernimt@gmail.com>
2019-08-01 16:07:07 +0200

    add tests for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_110740) do
+ActiveRecord::Schema.define(version: 2019_08_01_102120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 2019_07_31_110740) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "tests", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -32,6 +37,10 @@ ActiveRecord::Schema.define(version: 2019_07_31_110740) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birthday"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # context 'test user validations' do
-  #   user = User.create(email: '', password:'123456',password_confirmation:'123456') 
-  #   it 'can not have blank email' do
-  #     expect(user).to eq("Email can't be blank")
-  #   end
-  # end
+   context 'test user validations' do
+      let(:user){User.create(first_name: " ", last_name: "", gender: "", email:'', birthday: "", password: '12') }     
+      let(:blank_password){User.create(password: '') }     
+
+      it 'can not have blank first name' do
+       expect(user.errors.full_messages).to include("First name can't be blank")
+      end
+
+      it 'can not have blank last name' do
+       expect(user.errors.full_messages).to include("Last name can't be blank")
+      end
+
+      it 'can not have blank email' do
+       expect(user.errors.full_messages).to include("Email can't be blank")
+      end
+
+      it 'can not have blank birthdate' do
+       expect(user.errors.full_messages).to include("Birthday can't be blank")
+      end
+
+      it 'can not have blank gender' do
+       expect(user.errors.full_messages).to include("Gender can't be blank")
+      end
+
+      it 'can not have blank password' do
+       expect(blank_password.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it 'should have minimum of 6 lenght password' do
+       expect(user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+
+      it 'should belong to user' do
+        t = User.reflect_on_association(:posts)
+        expect(t.macro).to eq(:has_many)
+      end
+   end
+
+ 
 end
