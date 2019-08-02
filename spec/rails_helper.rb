166836f642739468911d8b71e8dd8dc57133e407
NeerNim <neernimt@gmail.com>
2019-08-03 00:28:26 +0200

    configure rspec for testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,53 +1,48 @@
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
-require 'database_cleaner'
-require 'capybara/rspec'
-require 'support/sessions_helper.rb'
+require "spec_helper"
+require "rspec/rails"
+# Add additional requires below this line. Rails is not loaded until this point!
+require "capybara/rspec"
+#require 'support/factory_bot'
 
 
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+Capybara.register_driver :selenium_chrome do |app|
+ Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
+Capybara.javascript_driver = :selenium_chrome
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
+  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
-
-  # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
 
-  config.include Devise::Test::IntegrationHelpers, :type => :request
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
-  config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
 
-  config.include SessionHelpers, type: :feature
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
 
+  # This block must be here, do not combine with the other `before(:each)` block.
+  # This makes it so Capybara can see the database.
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
