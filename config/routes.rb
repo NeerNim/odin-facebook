42de5680ec30076998c5681841118013a689772f
NeerNim <neernimt@gmail.com>
2019-08-05 01:17:04 +0200

    Configure route to the registration page as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'posts#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   resources :users, only: [:index]
-
   resources :posts
-
-  root to: "posts#index"
-  
 end
