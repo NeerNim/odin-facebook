1faf041af436f1acabc7b50b5c28f5ffac60747c
Afani Martin <afanimartin@localhost.localdomain>
2019-08-05 15:12:13 +0300

    Create models comment, like

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  has_many :likes
 
   validates :content, length: {maximum: 140}, presence: true
   validates :title, length: {minimum:10}, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  has_many :comments
+  has_many :likes, dependent: :destroy
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :birthday, presence: true
