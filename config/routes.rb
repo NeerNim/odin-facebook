648250bc83a5b251866705e9445b7453b7abfe26
Afani Martin <afanimartin@localhost.localdomain>
2019-08-05 16:20:29 +0300

    Add like controller so users can like posts

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
     end
   end
   resources :users, only: [:new, :create, :show, :index]
-  resources :posts
+  resources :posts do
+    resources :likes
+  end
 end
