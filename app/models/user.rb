4a9fb52c3169809ce666242015553e012e995cfe
Afani Martin <afanimartin@localhost.localdomain>
2019-08-06 13:38:42 +0300

    Delete post and comment when you delete user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts
-  has_many :comments
+  has_many :posts, ependent: :destroy
+  has_many :comments, ependent: :destroy
   has_many :likes, dependent: :destroy
 
   validates :first_name, presence: true
