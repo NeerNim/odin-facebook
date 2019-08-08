7f9b4c5666e1d786370e4ea8b6a97ad5ff96f7c5
NeerNim <neernimt@gmail.com>
2019-08-08 16:38:32 +0200

    add test for comment model

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,7 +2,5 @@ FactoryBot.define do
   factory :comment do
     title { "MyString" }
     body { "MyText" }
-    user { nil }
-    post { nil }
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let(:comment){FactoryBot.create(:comment, user_id:user.id, post_id:post.id)}
-  let(:no_title){FactoryBot.create(:comment, title:nil, user_id:user.id, post_id:post.id)}
-  let(:no_body){FactoryBot.create(:comment, body:nil, user_id:user.id, post_id:post.id)}
   let(:user){FactoryBot.create(:user)}
   let(:post){FactoryBot.create(:post, user_id:user.id)}
+  let(:comment){FactoryBot.create(:comment, user_id:user.id, post_id:post.id)}
+
 
   context "complete comment details" do
     it "is valid" do
@@ -13,15 +12,10 @@ RSpec.describe Comment, type: :model do
     end
   end
 
-  context "body is missing" do
+  context "comment belongs to post" do
     it "is invalid" do
-      no_body.valid?
-      expect(no_body.errors['body']).to include("Body can't be blank")
+      t = Comment.reflect_on_association(:user)
+      expect(t.macro).to eq(:belongs_to)
     end
-  end
-
-  it 'should belong to post' do
-    t = Comment.reflect_on_association(:user)
-    expect(t.macro).to eq(:belongs_to)
   end
 end
