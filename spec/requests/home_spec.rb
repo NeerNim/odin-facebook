c4f9951482f3ec129c9efad855308db33bad4bc3
NeerNim <neernimt@gmail.com>
2019-08-08 09:26:15 +0200

    add request test for homepage

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 
-RSpec.describe "Static pages", :type => :request do
+RSpec.describe "Home page", :type => :request do
+  let(:user) { FactoryBot.build(:user) }  
+  
+  it 'display homepage for signed in users' do 
+    sign_in user
+    get posts_path
+    expect(response).to render_template(:index) 
+    
+  end
 end
