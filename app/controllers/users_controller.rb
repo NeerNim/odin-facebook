2f90d583fad7e7067b716808c6f80df9d486b192
Afani Martin <afanimartin@localhost.localdomain>
2019-08-09 17:51:01 +0300

    Retrieve friends of the current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,16 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @friends = current_user.friendships
+  end
+
+  def create
+    @friend_id = Friendship.find(params[:friend_id])
+    @user = User.find(params[:id])
+    if friends.include(@friend_id)
+      flash.now[:notice] = "You already sent a request"
+    else
+      current_user.friendships.create(friend_id:@user.id)
+    end
   end
 end
