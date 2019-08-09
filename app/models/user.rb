aa09929f1390bc6d883f6345bb317e27c3209e07
Afani Martin <afanimartin@localhost.localdomain>
2019-08-09 12:38:54 +0300

    Create model friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,37 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships, dependent: :destroy
+  has_many :inverse_friendships, :class_name => "Friendships", :foreign_key => "friend_id", dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :birthday, presence: true
   validates :gender, presence: true
+
+
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array.compact
+  end
+
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  end
+
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
