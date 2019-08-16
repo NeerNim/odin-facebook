d054fc250a36bd25d45a2d28da670ec3b20189a3
NeerNim <neernimt@gmail.com>
2019-08-16 15:05:39 +0200

    add callback to list in the friends from bother users side

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all_except(current_user)    
+
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,21 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :likes, dependent: :destroy  
   has_many :friendships, dependent: :destroy
-  has_many :friends, :through => :friendships
-    
+  has_many :friends, through: :friendships
 
-  validates :first_name, presence: true, uniqueness: true
+  validates :first_name, presence: true
   validates :last_name, presence: true
   validates :birthday, presence: true
   validates :gender, presence: true
   scope :all_except, ->(user) { where.not(id: user) }
 
+   def pending_friends
+    Friendship.where(['friend_id = ? and status = ?', id, false])
+  end
 end
