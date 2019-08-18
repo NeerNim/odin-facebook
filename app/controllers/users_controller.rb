dff0cd525ee82b69bfb5c7483f5b54f38fac3f9d
NeerNim <neernimt@gmail.com>
2019-08-18 21:45:54 +0200

    add condition when both both users are friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
 
 
   def index
-    @users = User.all_except(current_user)    
+    @users = User.all_except(current_user)        
+    
   end
 
   def show
